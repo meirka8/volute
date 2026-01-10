9a2dfea3e9fbfffcab183a16da0fdb1f4aa25c9a
Meir Kanevskiy <meir.kanevskiy@gmail.com>
2026-01-10 21:39:35 +0200

    retry attempts

--- cvc-reviewer/src/App.tsx
+++ cvc-reviewer/src/App.tsx
@@ -8,7 +8,8 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      retry: false,
+      retry: 2,
+      retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 30000),
     },
   },
 });
